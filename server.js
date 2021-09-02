565ac1d68d13248646bb87a4b89205da8d928de5
darrenrector91 <darrenrector91@gmail.com>
2021-09-02 13:06:07 -0500

    updated port to 3000

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get("/manufacturers", function (req, res) {
   });
 });
 
-var server = app.listen(process.env.PORT || 5000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running..");
 });
 
